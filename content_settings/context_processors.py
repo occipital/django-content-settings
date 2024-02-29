a81fefdbaa842cf2200935d2a2887c3151e0c990
oduvan <a.lyabah@checkio.org>
2024-02-29 16:06:21 +0200

    #70 context_processor.settings

--- content_settings/context_processors.py
+++ content_settings/context_processors.py
@@ -1,5 +1,9 @@
-from .conf import content_settings as _content_settings
+from .conf import content_settings as _content_settings, settings as _settings
 
 
 def content_settings(request=None):
     return {"CONTENT_SETTINGS": _content_settings}
+
+
+def settings(request=None):
+    return {"SETTINGS": _settings}
